d90b70cf520d30d2a3f46f99804f61c5d3441769
TYSS <singhdeepa9555@gmail.com>
2018-06-13 14:59:51 +0530

    1st Commit

---
+++ src/test/java/com/restassured/tests/GetWeatherDetailsTest.java
@@ -0,0 +1,38 @@
+package com.restassured.tests;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import net.minidev.json.JSONObject;
+
+public class GetWeatherDetailsTest 
+{
+@Test
+public void getDetails()
+{
+	RestAssured.baseURI = "http://localhost:3000";
+	// Get the RequestSpecification of the request that you want to sent
+	// to the server. The server is specified by the BaseURI that we have
+	// specified in the above step.
+	RequestSpecification httpRequest = RestAssured.given();
+
+	// Make a GET request call directly by using RequestSpecification.get() method.
+	// Make sure you specify the resource name.
+	Response response = httpRequest.get("/customer");
+
+	// Response.asString method will directly return the content of the body
+	// as String.
+	 String resp = response.asString();
+     System.out.println("Response Body is =>  " + response.asString());
+	int statusCode = response.getStatusCode();
+	// Assert that correct status code is returned.
+	Assert.assertEquals(statusCode , 200 , "Correct status code returned");
+	 System.out.println("Status code  is =>  " + statusCode);
+}
+
+}
+
+
---
+++ src/test/java/com/restassured/tests/PostWeatherDetailsTest.java
@@ -0,0 +1,43 @@
+package com.restassured.tests;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import net.minidev.json.JSONObject;
+
+public class PostWeatherDetailsTest
+{
+	@Test
+	public void getDetails()
+	{
+		RestAssured.baseURI = "http://localhost:3000";
+		RequestSpecification request = RestAssured.given();
+		//JSONObject is a class that represents a simple
+		// JSON adding Key - Value pairs using the put method
+		JSONObject requestParams = new JSONObject();
+		System.out.println(requestParams);
+		System.out.println(request.post("/customer").asString());
+		requestParams.put("id", 101);
+		requestParams.put("FirstName", "allen");
+		requestParams.put("LastName", "joy"); 
+		requestParams.put("Phone number",  "1234565671");
+		requestParams.put("Email",  "xyz@gmail.com");
+		requestParams.put("Password", "password1");
+		System.out.println("**************************************************");
+		System.out.println(requestParams.get("Email"));
+		System.out.println(request.post("/register").asString());
+		// Add the Json to the body of the request
+		request.body(requestParams.toJSONString());
+		// Post the request and check the response
+		Response response = request.post("/register");
+		int statusCode = response.getStatusCode();
+		// Assert that correct status code is returned.
+		Assert.assertEquals(statusCode, "201");
+		String successCode = response.jsonPath().get("SuccessCode");
+		Assert.assertEquals( "Correct Success code was returned", successCode, "OPERATION_SUCCESS");
+
+	}
+}
